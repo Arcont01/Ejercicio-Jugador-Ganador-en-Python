78573e13ea797de9fdefba65d175d2c9b3769018
Omar <C.J_omar@hotmail.com>
2021-04-10 08:48:47 -0600

    cambios en el el calculo asi como implementacion de exceptiones y logs

--- exercise.py
+++ exercise.py
@@ -1,5 +1,7 @@
 import os.path
 import pathlib
+import logging
+from os import system
 
 class Players:
     def play(self, routeFile):
@@ -8,10 +10,8 @@ class Players:
             rounds = self.checkRounds(array)
             if rounds != 0:
                 response = self.checkContent(rounds,array)
-                if response == 0:
-                    print('Solo se pueden 2 jugadores')
             else:
-                print('Las rondas no pueden ser mayor a 10000')
+                raise Exception('Las rondas no pueden ser mayor a 10000')
 
     def openFile(self, routeFile):
         path = pathlib.Path(routeFile);
@@ -25,15 +25,17 @@ class Players:
                         file.close()
                         return array
                     except IOError:
-                        print('Error al tratar de leer archivo')
+                        raise Exception('Error al tratar de leer archivo')
                 else:
-                    print('el archivo debe de ser tipo TXT')
+                    raise TypeError('el archivo debe de ser tipo TXT')
         else:
-            print('No existe el archivo')
+            raise Exception('No existe el archivo')
 
     def checkRounds(self, content):
-        rounds = int(content[0])
-        content.pop(0)
+        rounds = content[0]
+
+        rounds = int(rounds)
+        content.pop(0)  
 
         if rounds > 10000:
            return 0
@@ -43,35 +45,43 @@ class Players:
     def checkContent(self, rounds, arrayContent):
         winnerPlayers = []
         differences = []
+
+        if rounds != len(arrayContent):
+            raise Exception('El numero de rounds definido es diferente a los que juegan los jugadores')
+
         for i in range(rounds):
            temp = arrayContent[i].split()
 
            if(len(temp) != 2):
-                return 0;
+               raise Exception('Las rondas son unicamente de 2 jugadores')
 
            firstPlayer = int(temp[0])
            secondPlayer = int(temp[1])
 
            if firstPlayer > secondPlayer:
                winnerPlayers.append(1)
+               tempDifference = firstPlayer - secondPlayer
            else:
                winnerPlayers.append(2)
+               tempDifference = secondPlayer - firstPlayer
+           
+           differences.append(tempDifference)
+           
 
-           tempDifference = firstPlayer - secondPlayer
-           differences.append(abs(tempDifference))
             
         maxPoint = max(differences)
         index = differences.index(maxPoint)
         winnerPlayer = winnerPlayers[index]
 
-        print('El jugador: ' + str(winnerPlayer) + ' gano, en la ronda ' + str(index + 1))
-        print('Con una diferencia de: ' + str(maxPoint))
-        return 1
+        print('Gano el jugador numero:', str(winnerPlayer))
+        print('Con una diferencia de puntos de:', str(maxPoint))
 
-
-routeFile = input('Por favor escriba el nombre del archivo: ')
-
-object = Players()
-object.play(routeFile)
+try:
+    routeFile = input('Por favor escriba el nombre del archivo: ')
+    object = Players()
+    object.play(routeFile)
+except Exception as e:
+    system('clear')
+    logging.error(e)
 
 
